4972d0d4a5afbe1607885423ffc7fe1383943c60
Alejandro-va <prof.jahz@gmail.com>
2023-01-25 14:04:43 -0300

    primero completo

--- App.js
+++ App.js
@@ -5,12 +5,35 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+
+import GamingImg from "./assets/images/misc/gaming.svg";
+//const GamingImg = require("./assets/images/misc/maming.svg");
 //import { Icon } from "react-native-vector-icons/Icon";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { AuthProvider } from "./src/context/AuthContext";
 //import AppNav from "./src/navigation/AppNav";
 
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          component={Main}
+          name="Main"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen component={Home} name="Home" />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export function Main({ navigation }) {
   return (
     <SafeAreaView
       style={{
@@ -20,7 +43,7 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <View>
+      <View style={{ marginTop: 30 }}>
         <Text
           style={{
             fontSize: 30,
@@ -32,7 +55,15 @@ export default function App() {
           Gameon
         </Text>
       </View>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <GamingImg
+          width={300}
+          height={300}
+          style={{ transform: [{ rotate: "-15deg" }] }}
+        />
+      </View>
       <TouchableOpacity
+        onPress={() => navigation.navigate("Home")}
         style={{
           backgroundColor: "#ad40af",
           padding: 20,
@@ -40,6 +71,7 @@ export default function App() {
           borderRadius: 5,
           flexDirection: "row",
           justifyContent: "space-between",
+          marginBottom: 50,
         }}
       >
         <Text style={{ fontWeight: "bold", color: "#fff", fontSize: 18 }}>
@@ -50,6 +82,21 @@ export default function App() {
     </SafeAreaView>
   );
 }
+
+const Home = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text>Home Component</Text>
+    </View>
+  );
+};
 
 /* const styles = StyleSheet.create({
   container: {
--- README.md
+++ README.md
@@ -11,3 +11,17 @@ creamos el archivo en el escritorio raiz:
 react-native.config.js
 luego en la consola el comando
 npx react-native-asset
+
+para imagenes svg
+https://github.com/kristerkari/react-native-svg-transformer
+npm install react-native-svg ---deprecate
+npx expo install react-native-svg@13.4.0
+npm i react-native-svg-transformer
+
+para navegacion
+https://reactnavigation.org/
+npm install @react-navigation/native
+npm install react-native-screens react-native-safe-area-context ---ESTE YA NO
+npx expo install react-native-safe-area-context@4.4.1 react-native-screens@~3.18.0
+npx pod-install ios
+npm install @react-navigation/native-stack
